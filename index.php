7d0d887e3dfe06cde9d75e371816c8682a1d6c9a
MikDem92 <mikhail.demikhovskiy@rwth-aachen.de>
2023-03-17 00:12:29 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Request Access Form</title>
+  </head>
+  <body>
+    <form action="https://test.org" method="post">
+      <label for="user">User:</label>
+      <input type="text" id="user" name="user" value="<?php echo htmlspecialchars($_GET['user']); ?>"><br><br>
+      <label for="site">Site:</label>
+      <input type="text" id="site" name="site" value="<?php echo htmlspecialchars($_GET['site']); ?>"><br><br>
+      <button type="submit">Request access</button>
+    </form>
+  </body>
+</html>
